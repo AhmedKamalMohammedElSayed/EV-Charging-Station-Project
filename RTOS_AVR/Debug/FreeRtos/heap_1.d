61f37205809aa2b8192102e12243437883ebb95d
unknown <ahmedkam412@gmail.com>
2025-01-18 00:19:53 +0200

    first

---
+++ RTOS_AVR/Debug/FreeRtos/heap_1.d
@@ -0,0 +1,64 @@
+FreeRtos/heap_1.d FreeRtos/heap_1.o: ../FreeRtos/heap_1.c \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../FreeRtos/include/FreeRTOS.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../Include/FreeRTOSConfig.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ D:\Atmel\ Software\ IDE\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom32a.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../FreeRtos/include/projdefs.h ../FreeRtos/include/portable.h \
+ ../FreeRtos/include/deprecated_definitions.h \
+ ../FreeRtos/include/portmacro.h ../FreeRtos/include/mpu_wrappers.h \
+ ../FreeRtos/include/task.h ../FreeRtos/include/list.h
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../FreeRtos/include/FreeRTOS.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../Include/FreeRTOSConfig.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+D:\Atmel\ Software\ IDE\7.0\Packs\atmel\ATmega_DFP\1.3.300\include/avr/iom32a.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\atmel\ software\ ide\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../FreeRtos/include/projdefs.h:
+
+../FreeRtos/include/portable.h:
+
+../FreeRtos/include/deprecated_definitions.h:
+
+../FreeRtos/include/portmacro.h:
+
+../FreeRtos/include/mpu_wrappers.h:
+
+../FreeRtos/include/task.h:
+
+../FreeRtos/include/list.h:
