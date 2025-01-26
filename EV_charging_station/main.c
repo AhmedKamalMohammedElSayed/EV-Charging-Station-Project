36c579c6ddde672b2f7b58a8672639ee4a635ff5
Assem Hussein <96780345+RushingBlast@users.noreply.github.com>
2025-01-26 20:10:16 +0200

    temp commit

--- EV_charging_station/main.c
+++ EV_charging_station/main.c
@@ -5,15 +5,28 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#define IDLE 0
+#define CHARGING 1
+#define STOP 2
+
+
+
 #define prescaler 128
 
 #define ADC_THRESHOLD 100
+#define CONNECTION_THRESHOLD 1022
+
+uint8_t is_vehicle_connected = 0;
 
 TaskHandle_t xADC_Task_Handle = NULL;
 TaskHandle_t xOverCurrent_Task_Handle = NULL;
 
 volatile uint8_t overcurrent_flag = 0;
 
+#define VEHICLE_CHECK_PERIOD 100 // in ms
+#define VEHICLE_CHECK_PIN 2
+
+uint8_t charging_state = 0;
 
 // Function to initialize UART for debugging (optional)
 void UART_Init(unsigned int baud) {
@@ -49,10 +62,20 @@ void ADC_Task(void *pvParameters) {
         // Read the ADC value from channel 1 (ADC1)
         adc_OC_value = ADC_READ(1);
 
+        adc_vehicle_connected = ADC_READ(VEHICLE_CHECK_PIN);
+
         // Check if the ADC value is above the threshold
         if (adc_OC_value > ADC_THRESHOLD) {
             // Set the overcurrent flag
             xTaskNotifyGive(xOverCurrent_Task_Handle);
+        }
+
+        // Check if a load is plugged in
+        if (adc_vehicle_connected < CONNECTION_THRESHOLD) {
+            // set vehicle connected flag
+            is_vehicle_connected = 1;
+        } else {
+            is_vehicle_connected = 0;
         }
 
         // Save ADC value into shared memory (could be used in UART task)
@@ -113,12 +136,74 @@ void Overcurrent_Task(void *pvParameters) {
     }
 }
 
+void prd_VehicleCheck(void *pvParameters) {
+    TickType_t xLastWakeTime = xTaskGetTickCount(); 
+    
+    uint16_t adc_load_value = 0;
+    
+    while (1) {
+        
+
+        // Check if the ADC value is above the threshold
+        if (adc_load_value < CONNECTION_THRESHOLD) {
+            is_vehicle_connected = 1;
+        } else {
+            is_vehicle_connected = 0;
+        }
+
+        
+
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(VEHICLE_CHECK_PERIOD));
+    }
+}
+
+
+void prd_LED_MANAGEMENT(void *pvParameters) {
+    TickType_t xLastWakeTime = xTaskGetTickCount(); 
+    while (1) {
+        if (is_vehicle_connected) {
+            PORTC |= (1 << PC0); // Set PC0 to high
+        } else {
+            PORTC &= ~(1 << PC0);  // Set PC0 to low
+        }
+
+        switch (charging_state) {
+            case IDLE:
+                PORTC &= ~(1 << PC0);  // Set PC0 to low
+                break;
+            case CHARGING:
+                PORTC |= (1 << PC0); // Set PC0 to high
+                break;
+            case STOP:
+                PORTC &= ~(1 << PC0);  // Set PC0 to low
+                break;
+            default:
+                break;
+        }
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(200));
+    }
+}
+
+
+void prd_Buttons(void *pvParameters) {
+    TickType_t xLastWakeTime = xTaskGetTickCount(); 
+    while (1) {
+        // If start is pressed
+        if(is_vehicle_connected)
+        if (PINB & (1 << PB0)) {
+            
+        }
+        }
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(200));
+    }
+}
+
 int main(void) {
     // Initialize the ADC and UART
     ADC_INIT(prescaler, AREF);
     UART_Init(9600);
 
-    DDRC |= (1 << PC0); // Set PC0 as output
+    DDRC |= (1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5); // Set PC0 as output
 
     // Create the ADC reading task
     xTaskCreate(ADC_Task, "ADC Task", 128, NULL, 1, &xADC_Task_Handle);
